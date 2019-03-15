63245acd180ce0741d573b2070e8454890cfd44b
Oscar Caballero <ocaballeror@tutanota.com>
2019-03-15 10:29:27 +0100

    Python 2.7 support

--- launch_jenkins.py
+++ launch_jenkins.py
@@ -1,6 +1,10 @@
 """
 Launch a jenkins job and wait for it to finish.
 """
+from __future__ import print_function
+from __future__ import unicode_literals
+from __future__ import absolute_import
+
 import argparse
 import json
 import sys
