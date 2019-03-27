5213ce4355058261e9e7d1653f7bf6345b9fbb67
Oscar Caballero <ocaballeror@tutanota.com>
2019-03-27 14:51:19 +0100

    Create terminal_size fixture for py2 compatibility

--- tests/test_helper.py
+++ tests/test_helper.py
@@ -2,6 +2,8 @@ import os
 import time
 from collections import namedtuple
 
+import pytest
+
 from launch_and_wait import show_progress
 
 
@@ -47,12 +49,24 @@ def assert_empty_progress(capsys):
     assert not outerr.out
 
 
-def set_get_terminal_size(monkeypatch):
+@pytest.fixture(scope='function')
+def terminal_size(monkeypatch):
+    """
+    Set a fake os.get_terminal_size() function that returns (30, 30).
+    """
     def fake_terminal_size(*args, **kwargs):
         return Size(30, 30)
 
     Size = namedtuple('terminal_size', 'columns rows')
-    monkeypatch.setattr(os, 'get_terminal_size', fake_terminal_size)
+    has_func = hasattr(os, 'get_terminal_size')
+    if not has_func:
+        os.get_terminal_size = fake_terminal_size
+    else:
+        monkeypatch.setattr(os, 'get_terminal_size', fake_terminal_size)
+    yield
+
+    if not has_func:
+        del os.get_terminal_size
 
 
 def raise_error(*args, **kwargs):
