9b66791859b5c2ef3f21aba2b89e51ad96eafb46
Oscar Caballero <ocaballeror@tutanota.com>
2019-05-22 14:01:44 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="jenkins_launch",
+    description="Launch a jenkins job and wait for it to finish",
+    version="1.0",
+    author="Oscar Caballero",
+    author_email="ocaballeror@tutanota.com"
+)
