b6941c57a7c0f61b991f6897c0b93c60be79f499
Oscar Caballero <ocaballeror@tutanota.com>
2019-05-27 12:38:06 +0200

    Bump build version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="jenkins_launch",
     description="Launch a jenkins job and wait for it to finish",
-    version="1.0",
+    version="2.0",
     author="Oscar Caballero",
     author_email="ocaballeror@tutanota.com",
     packages=['launch_jenkins'],
